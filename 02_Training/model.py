dfdbed2fb17d63a1b6e86b3182aaa3063aee89ab
kritomas <Unknown>
2025-03-28 22:33:03 +0100

    Added initial interface.

--- 02_Training/model.py
+++ 02_Training/model.py
@@ -1,4 +1,4 @@
-import sqlite3, pickle, pandas as pd
+import sqlite3, pickle, pandas as pd, matplotlib.pyplot as plt, seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
@@ -8,7 +8,14 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 import config
 
 with sqlite3.connect(config.conf["db"]["db_path"]) as conn:
-	data = pd.read_sql("select line_name, direction, unixepoch(start_timestamp) as start_timestamp, (unixepoch(timestamp) - unixepoch(start_timestamp)) as duration, latitude, longitude from Vehicle;", conn)
+	data = pd.read_sql("select line_name, direction, unixepoch(start_timestamp) as start_timestamp, (unixepoch(timestamp) - unixepoch(start_timestamp)) as duration, latitude, longitude from Vehicle where position_state in ('at_stop', 'on_track', 'off_track');", conn)
+	print(data.head())
+
+#data_numeric = data.select_dtypes(include=["float64", "int64"])
+#correlation_matrix = data_numeric.corr()
+#plt.figure(figsize=(12, 10))
+#sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap="viridis")
+#plt.show()
 
 categorical_cols = ["line_name", "direction"]
 column_trans = ColumnTransformer([
@@ -18,7 +25,7 @@ column_trans = ColumnTransformer([
 input_attr = ["line_name", "direction", "start_timestamp", "duration"]
 output_attr = ["latitude", "longitude"]
 
-X_train, X_test, y_train, y_test = train_test_split(data[input_attr], data[output_attr], test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(data[input_attr], data[output_attr], test_size=0.05)
 
 model = Pipeline([("trans", column_trans), ("rfr", RandomForestRegressor(n_jobs=-1))])
 model.fit(X_train, y_train)
