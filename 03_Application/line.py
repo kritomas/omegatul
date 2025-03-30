d7046421198af83c8aeb95fa567fe3bbb3edad13
kritomas <Unknown>
2025-03-30 21:05:36 +0200

    Added line fetching thing.

---
+++ 03_Application/line.py
@@ -0,0 +1,37 @@
+import requests
+import config
+
+def fetch(line):
+	line = line.split("/", 1)
+	headers = {"X-Access-Token": config.conf["api"]["token"], "User-Agent": "Omegatul"}
+	vehicle_params = {"routeType": ["bus", "trolleybus"], "routeShortName": line[0]}
+
+	response = requests.get("https://api.golemio.cz/v2/public/vehiclepositions", headers=headers, params=vehicle_params)
+	response.raise_for_status()
+	data = response.json()
+	result = []
+	for vehicle in data["features"]:
+		trip_id = vehicle["properties"]["gtfs_trip_id"]
+		retrying = True
+		while retrying:
+			try:
+				response = requests.get(f"https://api.golemio.cz/v2/vehiclepositions/{trip_id}", headers=headers)
+				response.raise_for_status()
+				retrying = False
+				trip = response.json()
+				if trip["properties"]["trip"]["gtfs"]["trip_headsign"] == line[1]:
+					result.append(vehicle)
+			except requests.HTTPError as error:
+				if error.response.status_code == 429: # 401=Bad Token, 429=Rate Limit
+					time.sleep(1)
+				elif error.response.status_code == 404:
+					retrying = False
+				else:
+					raise
+					retrying = False
+					#print(error)
+			except Exception as error:
+				raise
+				retrying = False
+				#print(error)
+	return result
