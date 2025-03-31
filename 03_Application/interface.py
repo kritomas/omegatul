807a5d8e1bc4aebb557b6c43ed112c1fb7fd0cee
kritomas <Unknown>
2025-03-31 19:03:39 +0200

    Added display of individual vehicles.

--- 03_Application/interface.py
+++ 03_Application/interface.py
@@ -2,6 +2,20 @@ import time, dateutil
 import line
 
 class Interface:
+	def _formatVehicle(self, vehicle):
+		info = ""
+		info += vehicle["properties"]["vehicle_id"] + ": "
+		info += "Currently at " + str(vehicle["geometry"]["coordinates"][0]) + "E " + str(vehicle["geometry"]["coordinates"][1]) + "N, "
+		info += "started at " + vehicle["properties"]["gtfs"]["properties"]["trip"]["start_timestamp"]
+		duration = int(time.mktime(dateutil.parser.parse(vehicle["properties"]["gtfs"]["properties"]["last_position"]["origin_timestamp"]).timetuple())) -int(time.mktime(dateutil.parser.parse(vehicle["properties"]["gtfs"]["properties"]["trip"]["start_timestamp"]).timetuple()))
+		duration_minutes = int(duration / 60)
+		duration_seconds = duration % 60
+		if duration > 0:
+			info += " (" + str(duration_minutes) + "min " + str(duration_seconds) + "s into its journey)"
+		else:
+			info += " (not on track yet)"
+		return info
+
 	def exit(self):
 		self.running = False
 	def line(self):
@@ -11,22 +25,17 @@ class Interface:
 			raise ValueError("Incorrectly formatted line")
 		vehicles = line.fetch(number, direction)
 		for v in vehicles:
-			info = v["properties"]["vehicle_id"] + ": "
-			info += "Currently at " + str(v["geometry"]["coordinates"][0]) + "E " + str(v["geometry"]["coordinates"][1]) + "N, "
-			info += "started at " + v["properties"]["gtfs"]["properties"]["trip"]["start_timestamp"]
-			duration = int(time.mktime(dateutil.parser.parse(v["properties"]["gtfs"]["properties"]["last_position"]["origin_timestamp"]).timetuple())) -int(time.mktime(dateutil.parser.parse(v["properties"]["gtfs"]["properties"]["trip"]["start_timestamp"]).timetuple()))
-			duration_minutes = int(duration / 60)
-			duration_seconds = duration % 60
-			if duration > 0:
-				info += " (" + str(duration_minutes) + "min " + str(duration_seconds) + "s into its journey)"
-			else:
-				info += " (not on track yet)"
-			print(info)
+			print(self._formatVehicle(v))
+	def vehicle(self):
+		vehicle_id = input("Enter vehicle ID: ")
+		vehicle = line.fetchOne(vehicle_id)
+		print(self._formatVehicle(vehicle))
 
 	def __init__(self):
 		self.commands = {
 			"exit": self.exit,
-			"line": self.line
+			"line": self.line,
+			"vehicle": self.vehicle
 		}
 
 	def start(self):
--- 03_Application/line.py
+++ 03_Application/line.py
@@ -35,3 +35,36 @@ def fetch(number, direction):
 				retrying = False
 				#print(error)
 	return result
+
+def fetchOne(vehicle_id):
+	headers = {"X-Access-Token": config.conf["api"]["token"], "User-Agent": "Omegatul"}
+	vehicle_params = {"scopes": ["info"]}
+
+	response = requests.get(f"https://api.golemio.cz/v2/public/vehiclepositions/{vehicle_id}", headers=headers, params=vehicle_params)
+	response.raise_for_status()
+	vehicle = response.json()
+	vehicle["properties"] = vehicle
+	vehicle["properties"]["vehicle_id"] = vehicle_id
+	trip_id = vehicle["gtfs_trip_id"]
+	retrying = True
+	while retrying:
+		try:
+			response = requests.get(f"https://api.golemio.cz/v2/vehiclepositions/{trip_id}", headers=headers)
+			response.raise_for_status()
+			retrying = False
+			trip = response.json()
+			vehicle["gtfs"] = trip
+		except requests.HTTPError as error:
+			if error.response.status_code == 429: # 401=Bad Token, 429=Rate Limit
+				time.sleep(1)
+			elif error.response.status_code == 404:
+				retrying = False
+			else:
+				raise
+				retrying = False
+				#print(error)
+		except Exception as error:
+			raise
+			retrying = False
+			#print(error)
+	return vehicle
