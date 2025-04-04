4d8b80ac3fd971d5ce03a703b6b28883136aab29
kritomas <Unknown>
2025-04-04 11:07:40 +0200

    Fixed crash when parsing .csv on windows.
    Fixes: GHI-1

--- 03_Application/stop.py
+++ 03_Application/stop.py
@@ -5,7 +5,7 @@ lookup_id2name = {}
 lookup_id2longitude = {}
 lookup_id2latitude = {}
 
-with open("stops.csv", newline="") as file:
+with open("stops.csv", newline="", encoding="utf-8") as file:
 	reader = csv.reader(file, delimiter=',', quotechar='"')
 	for row in reader:
 		lookup_id2name[row[0]] = row[1]
